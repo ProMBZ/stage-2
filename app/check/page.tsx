d06c68811433156b26ad7c255ced57142b0f4891
Pro MBZ <muhammadzohaib1415@gmail.com>
2024-07-05 13:52:48 +0500

    Describe your changes

---
+++ app/check/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CheckMail from '../components/CheckMail'
+
+const Page = () => {
+  return (
+    <div>
+      <CheckMail />
+    </div>
+  )
+}
+
+export default Page
